03e16db2fcde3148716d74d2317069e6955e22f7
MetalStorm15 <metalstorm15@163.com>
2021-03-28 12:34:06 +0800

    生成网页

--- scripts/maker.py
+++ scripts/maker.py
@@ -1,15 +1,31 @@
 import os
 import re
 
-def make_sites(poem_number='all'):
-    if not poem_number == 'all':
+def make_sites(poem_numbers='all'):
+    if poem_numbers == 'all':
+        poem_numbers = os.listdir('../contents')
+    
+    for poem_number in poem_numbers:
         with open('../contents/{0}/name.txt'.format(poem_number)) as file:
             poem_name = file.read()
 
         with open('../contents/{0}/body.txt'.format(poem_number)) as file:
-            poem_body = file.readline()
+            poem_body = file.readlines()
 
-        for line in poem_body:
-            line = '<p>' + line + '</p>'
-
+        with open('model1.txt') as file:
+            model = file.read()
         
+        model = re.split('split', model)
+        
+        with open ('../{0}.html'.format(poem_number), 'w') as file:
+            file.write(model[0])
+            file.write(poem_name)
+            file.write(model[1])
+            file.write(poem_name)
+            file.write(model[2])
+            for line in poem_body:
+                if not line == '\n':
+                    line = '<p>\n' + line + '</p>\n'
+                    file.write(line)
+            file.write(model[3])
+
