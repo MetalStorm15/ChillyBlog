6a0df993dd4d91a29846737b1b191bac7058abc0
MetalStorm15 <metalstorm15@163.com>
2021-07-31 20:53:36 +0800

    重置Poetry.md文件的目录形式为链接，修改诗名中全半角混用

--- scripts/maker.py
+++ scripts/maker.py
@@ -25,22 +25,33 @@ def make_sites(texts_dir, model_loc, output, catalogue):
 
     This function is used for making the sites showing the powtry.
     """
+
+    # 获取文件名
     poems = os.listdir(texts_dir)
+
+    # 制作网页文件
     for poem in poems:
+        # 读取诗名
         with open(texts_dir + poem + '/name.txt', encoding='utf-8') as file:
             poem_name = file.read()
 
+        # 读取诗的正文
         with open(texts_dir + poem + '/body.txt', encoding='utf-8') as file:
             poem_body = file.readlines()
 
+        # 读取模板文件
         with open(model_loc, encoding='utf-8') as file:
             model = file.read()
+
         body = ''
+
+        # 制作诗词正文字符串
         for line in poem_body:
             if not line == '\n':
                 line = '\t\t<p>' + line[:-1] + '</p>\n'
                 body += line
 
+        # 获取跳转所需的链接
         if poem == '0':
             previous_page = catalogue
             n = int(poem) + 1
@@ -55,12 +66,14 @@ def make_sites(texts_dir, model_loc, output, catalogue):
             next_page = str(n) + '.html'
             previous_page = str(p) + '.html'
 
+        # 填充模板，制作网页文件
         model = model.replace('the title', poem_name)
         model = model.replace('the body', body)
         model = model.replace('the previous', previous_page)
         model = model.replace('the next', next_page)
         model = model.replace('the catalogue', catalogue)
 
+        # 写入内容
         with open(output + poem + '.html', 'w', encoding='utf-8') as file:
             file.write(model)
 
@@ -75,19 +88,32 @@ def make_catalogue(texts_dir, model_loc, catalogue):
 
     This function is used for making the contents file.
     """
+
+    # 读取文件夹名
     file_names = os.listdir(texts_dir)
     files = []
+
+    # 将字符串文件名转为整数格式
     for f_name in file_names:
         files.append(int(f_name))
+
+    # 重新排序
     files.sort()
     contents_list = ''
+
+    # 制作目录
     for file in files:
         with open(texts_dir + str(file) + '/name.txt', encoding='utf-8') as f:
             name = f.read()
             name = name.replace('\n', '')
+            
         contents_list += '\t\t\t<li><a href="./{0}.html">{1}</a></li>\n'.format(str(file), name)
+
+    # 读取模板
     with open(model_loc, encoding='utf-8') as f:
         model = f.read()
+
+    # 写入填充后的模板
     with open(catalogue, 'w', encoding='utf-8') as f:
         model = model.replace('the list', contents_list)
         f.write(model)
@@ -100,27 +126,62 @@ def make_md(texts_dir, output='../Poetry.md'):
 
     To create a markdown file which contains all the contents of the poetry.
     """
+
+    # 读取文件夹
     poems = os.listdir(texts_dir)
     poem_numbers = []
+
+    # 文件名转换为数字格式
     for poem in poems:
         poem_numbers.append(int(poem))
+
+    # 重新排序
     poem_numbers.sort()
     poems_n = []
+
+    # 转回字符串格式
     for poem_number in poem_numbers:
         poems_n.append(str(poem_number))
+
     poems = poems_n[:]
+
+    # 写入诗词正文
     with open(output, 'w', encoding='utf-8') as f:
+        # 写入大标题
         f.write('# Poetry\n\n')
-        f.write('[TOC]\n\n')
+        # 写入目录
         for poem in poems:
+            # 读取诗名
             with open(texts_dir + poem + '/name.txt', encoding='utf-8') as file:
                 poem_name = file.read()
+            
+            # Markdown目录索引
+            index = poem_name.replace('（', '')
+            index = index.replace('）', '')
+
+            # 写入目录
+            f.write('+ [{0}](#{1})\n'.format(poem_name, index))
+
+        f.write('\n')
+
+        # 写入诗词正文
+        for poem in poems:
+            # 读取诗名
+            with open(texts_dir + poem + '/name.txt', encoding='utf-8') as file:
+                poem_name = file.read()
+
+            # 写入诗名
             f.write('## {0} \n\n'.format(poem_name))
 
+            # 读取诗正文
             with open(texts_dir + poem + '/body.txt', encoding='utf-8') as file:
                 poem_body = file.readlines()
+
+            # 写入诗正文
             for line in poem_body:
                 f.write('{0}\n'.format(line))
+
+            # 写入分隔符
             f.write('\n-----\n\n')
 
 
@@ -137,8 +198,11 @@ def make(texts_dir, model_1, model_2, output, catalogue, md_file='../Poetry.md')
 
     This is a function to build websites files for poetry.
     """
+    # 制作目录文件
     make_catalogue(texts_dir, model_2, catalogue)
+    # 制作网页文件
     make_sites(texts_dir, model_1, output, catalogue)
+    # 制作Markdown文件
     make_md(texts_dir, md_file)
 
 
