0413972241069511ac7a5e06cc4048253d5afaa8
MetalStorm15 <metalstorm15@163.com>
2022-02-04 13:25:23 +0800

    添加诗歌，更改主题

---
+++ content/日记/English  Learning/My-Lack-of-Self-discipline.md
@@ -0,0 +1,17 @@
+---
+Title: My Lack of Self-discipline
+Author: Chilly
+Date: 2021-7-8
+Category: 日记
+Tags: English Learning
+---
+
+Yesterday, 7th July, I didn't keep anyting in my English diary and I almost learned nothing except reading some pages of *The Lord of the Rings*. I didn't practice handwriting and read Ancient Chinese literature, which I consider as the things of the most importance. Plainly, I am not a disciplined learner. 
+
+I have to admit the fact of my lack of willpower, and it's also true that I should not be too self-critical, which will not be conducive to my further evolution. It's in my nature and I have to accept it or I will get punishment given by god (or sky in Chinese expression).
+
+To resolve a problem completely, it is the first step to admit and accept the fact. If people are unlikely to affect the nonexistance, then, a fotiori, people could not remedy a problem which they refused to concede.
+
+So I know I am not self-disciplined now, and I do not think it is shame, moreover, I do not think it's a fault to have disadvantages. The world is so critical that people should not express or even have any cowardliness and sorrow. We are all forced to be strong and positive and the negative emotions are disgraceful.
+
+The fucking world!
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@ SITENAME = '支离的博客'
 SITEURL = '.'
 
 PATH = 'content'
-OUTPUT_PATH = './'
+OUTPUT_PATH = './output'
 TIMEZONE = 'Asia/Shanghai'
 DEFAULT_LANG = 'Chinese (Simplified)'
 
@@ -18,15 +18,18 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+         ('支离的诗歌网站', 'https://MetalStorm15.github.io/ChillyPoetry/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('知乎：支离-Chilly', 'https://www.zhihu.com/people/jin-shu-feng-bao-56'),
+          ('微博：支离-Chilly', 'https://m.weibo.cn/u/7418468313?uid=7418468313&t=0&luicode=10000011&lfid=100103type%3D1%26q%3D%E6%94%AF%E7%A6%BB-Chilly'),)
 
-DEFAULT_PAGINATION = 3
+DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-THEME = './pelican-themes/brownstone'
+THEME = './pelican-theme/backdrop'
+PAGINATED_DIRECT_TEMPLATES = ('categories', 'archives')
+EMAIL = 'metalstorm15@163.com'
+BACKDROP_IMAGE = "http://p6.itc.cn/q_70/images03/20200706/ecfd1a30a0a447a0b776ee8a41b21a7c.jpeg"
