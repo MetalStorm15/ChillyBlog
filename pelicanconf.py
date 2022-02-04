07fe803c0c7ccc428f7d52660e0b55f23b492937
MetalStorm15 <metalstorm15@163.com>
2022-02-04 13:55:16 +0800

    修改背景图片

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,7 +24,7 @@ LINKS = (('Pelican', 'https://getpelican.com/'),
 SOCIAL = (('知乎：支离-Chilly', 'https://www.zhihu.com/people/jin-shu-feng-bao-56'),
           ('微博：支离-Chilly', 'https://m.weibo.cn/u/7418468313?uid=7418468313&t=0&luicode=10000011&lfid=100103type%3D1%26q%3D%E6%94%AF%E7%A6%BB-Chilly'),)
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = 20
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
@@ -32,4 +32,4 @@ RELATIVE_URLS = True
 THEME = './pelican-theme/backdrop'
 PAGINATED_DIRECT_TEMPLATES = ('categories', 'archives')
 EMAIL = 'metalstorm15@163.com'
-BACKDROP_IMAGE = "http://p6.itc.cn/q_70/images03/20200706/ecfd1a30a0a447a0b776ee8a41b21a7c.jpeg"
+BACKDROP_IMAGE = "/images/bg.jpg"
